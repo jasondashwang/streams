121c0f3f87330648c2c2b01eb4e0c852d77e4b10
Samuel Lam <samuelkinlam@gmail.com>
2016-08-17 12:33:40 -0400

    small changes

--- www/js/camera/camera.js
+++ www/js/camera/camera.js
@@ -1,31 +1,9 @@
 'use strict';
 
-angular.module('main').controller('CameraCtrl', function ($rootScope, $scope, $state, $cordovaCamera, Upload) {
+angular.module('main').controller('CameraCtrl', function ($rootScope, $scope, $state, $cordovaCamera, CameraFactory) {
 
   $scope.changeStatus = function () {
     $rootScope.groupLoggedIn = !($rootScope.groupLoggedIn);
-  };
-
-  $scope.uploadImage = function(imageData) {
-    if (!$rootScope.profile) return;
-
-    var file = Upload.dataUrltoBlob(imageData, $rootScope.profile.uid);
-    var uploadPic = storageRef.child('pictures/' + file.name).put(file);
-
-    uploadPic.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
-      function(snapshot) {
-        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log('Upload is ' + progress + '% done');
-      }, function(err) {
-        console.error(err);
-      }, function() {
-        // Upload completed successfully, now we can get the download URL
-        var downloadURL = uploadPic.snapshot.downloadURL;
-        $scope.result = downloadURL;
-        firebase.database().ref().child('users').child(name).child('photoUrl').set(downloadURL);
-    });
-
   };
 
 	$scope.takeImage = function() {
@@ -43,7 +21,7 @@ angular.module('main').controller('CameraCtrl', function ($rootScope, $scope, $s
 
 	    $cordovaCamera.getPicture(options).then(function(imageData) {
 	        $scope.srcImage = "data:image/jpeg;base64," + imageData;
-          $scope.uploadImage($scope.srcImage);
+          CameraFactory.uploadImage($scope.srcImage);
 	    }, function(err) {
 	        console.error('Camera Error', err);
 	    });
--- www/js/common/factories/camera.factory.js
+++ www/js/common/factories/camera.factory.js
@@ -1,39 +1,29 @@
 angular.module('main')
-.factory('CameraFactory', function($rootScope) {
+.factory('CameraFactory', function($rootScope, Upload) {
 
   var CameraFactory = {};
 
   var ref = firebase.database().ref();
 
-  CameraFactory.uploadImage = function(image) {
+  CameraFactory.uploadImage = function(imageData) {
+    if (!$rootScope.profile) return;
 
-      var storageRef = firebase.storage().ref();
-      var imageRef = storageRef.child('picture.jpg');
-      var pictureImageRef = storageRef.child('images/picture.jpg');
-      var file = Upload.dataUrltoBlob(image, 'picture.jpg');
+    var file = Upload.dataUrltoBlob(imageData, $rootScope.profile.uid);
+    var uploadPic = storageRef.child('pictures/' + file.name).put(file);
 
-      var uploadPic = storageRef.child('pictures/' + file.name).put(file);
-
-      // Listen for state changes, errors, and completion of the upload.
-      uploadPic.on(firebase.storage.TaskEvent.STATE_CHANGED, function(snapshot) {
-          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-          let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log('Upload is ' + progress + '% done');
-        }, function(error) {
-          console.error(error);
+    uploadPic.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+      function(snapshot) {
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+      }, function(err) {
+        console.error(err);
       }, function() {
         // Upload completed successfully, now we can get the download URL
         var downloadURL = uploadPic.snapshot.downloadURL;
-        return downloadURL;
-
-        // var newPictureKey = ref.child('groups').child('groupCollage').push().key;
-
-        // firebase.database().ref('groups/' + $rootScope.groupId + '/groupCollage/' + newPictureKey).set({
-        //   pictureUrl: downloadURL,
-        //   location: 'some location',
-        //   owner: $rootScope.profile
-        // });
-      });
+        // $scope.result = downloadURL;
+        // firebase.database().ref().child('groups/' + $rootScope.SOMEGROUPID).child('photoUrl').set(downloadURL);
+    });
   };
 
   return CameraFactory;
