44807431b27cf98717425b960a36cb75910eb448
Jason Wang <jasonwang67@gmail.com>
2016-08-17 14:23:53 -0400

    log out button and settings files

--- www/js/profile/profile.js
+++ www/js/profile/profile.js
@@ -1,6 +1,13 @@
 'use strict';
 
-angular.module('main').controller('ProfileCtrl', function ($scope, $rootScope) {
+angular.module('main').controller('ProfileCtrl', function ($scope, $state, $rootScope) {
   $scope.profile = $rootScope.profile;
+
+  $scope.logOut = function(){
+    $rootScope.loggedIn = false;
+    $rootScope.groupLoggedIn = false;
+    $rootScope.profile = undefined;
+    $state.go('tab.login');
+  };
 });
 
