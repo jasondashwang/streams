ffb2a2b69353e5497b6dc73d4468c929ca9dbaf9
Jason Wang <jasonwang67@gmail.com>
2016-08-17 17:00:42 -0400

    new group

--- www/js/common/factories/group.factory.js
+++ www/js/common/factories/group.factory.js
@@ -2,6 +2,11 @@ angular.module('main')
 .factory('GroupFactory', function ($q, $rootScope) {
 
   const GroupFactory = {};
+  const alphanumeric_unique = function () {
+      return Math.random().toString(36).split('').filter( function(value, index, self) {
+          return self.indexOf(value) === index;
+      }).join('').substr(2,6);
+  };
 
   // const user = $rootScope.profile;
   const ref = firebase.database().ref();
@@ -10,20 +15,21 @@ angular.module('main')
   GroupFactory.createGroup = function (groupDetails) {
 
     // get a new key for the group
-    const newGroupKey = ref.child('activeGroups').push().key;
+    const newGroupKey = alphanumeric_unique();
+    // const newGroupKey = ref.child('groups').push().key;
 
     const groupPostData = {
       name: groupDetails.name,
       members: {},
       active: true,
-      groupCode: $rootScope.profile.email,
-      media: {}
+      media: {},
+      leaderId: $rootScope.profile.uid
     };
 
     $rootScope.profile.activeCode = newGroupKey;
     groupPostData.members[$rootScope.profile.uid] = $rootScope.profile;
     const updates = {};
-    updates[`/activeGroups/${newGroupKey}`] = groupPostData;
+    updates[`/groups/${newGroupKey}`] = groupPostData;
     $rootScope.profile.isLeader = true;
     updates[`/users/${$rootScope.profile.uid}`] = $rootScope.profile;
 
@@ -31,14 +37,14 @@ angular.module('main')
   };
 
   GroupFactory.addMember = function (groupCode) {
-    var groupRef = ref.child('activeGroups/' + groupCode + '/members/' + $rootScope.profile.uid);
+    var groupRef = ref.child('groups/' + groupCode + '/members/' + $rootScope.profile.uid);
     $rootScope.profile.activeCode = groupCode;
     return groupRef.update($rootScope.profile);
   };
 
   GroupFactory.fetchCurrentGroup = function () {
     var userGroupRef = $q.defer();
-    ref.child('activeGroups/' + $rootScope.profile.activeCode).on('value', function(snapshot){
+    ref.child('groups/' + $rootScope.profile.activeCode).on('value', function(snapshot){
           userGroupRef.resolve(snapshot.val());
         }, function(errorObject){
           userGroupRef.reject(errorObject.code);
@@ -49,7 +55,7 @@ angular.module('main')
   GroupFactory.fetchMedia = function () {
     var mediaObjects = $q.defer();
     console.log('The profile ', $rootScope.profile)
-    ref.child('activeGroups/' + $rootScope.profile.activeCode + '/media').on('value', function(snapshot){
+    ref.child('groups/' + $rootScope.profile.activeCode + '/media').on('value', function(snapshot){
       mediaObjects.resolve(snapshot.val());
     }, function(errorObject){
       mediaObjects.reject(errorObject.code);
