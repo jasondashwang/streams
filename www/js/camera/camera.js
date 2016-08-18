b5fed108b289d3fbebdc0915545d77faeb4e4731
Seon Joong Kim <seonjoongkim@college.harvard.edu>
2016-08-17 22:05:33 -0400

    video works

--- www/js/camera/camera.js
+++ www/js/camera/camera.js
@@ -1,30 +1,58 @@
 'use strict';
 
-angular.module('main').controller('CameraCtrl', function ($rootScope, $scope, $state, $cordovaCamera, CameraFactory) {
+angular.module('main').controller('CameraCtrl', function ($rootScope, $scope, $state, CameraFactory, $cordovaCapture, $cordovaCamera, Upload) {
 
-  $scope.changeStatus = function () {
-    $rootScope.groupLoggedIn = !($rootScope.groupLoggedIn);
-  };
-
-	$scope.takeImage = function() {
-	    var options = {
-	        quality: 80,
-	        destinationType: Camera.DestinationType.DATA_URL,
-	        sourceType: Camera.PictureSourceType.CAMERA,
-	        allowEdit: true,
-	        encodingType: Camera.EncodingType.JPEG,
-	        targetWidth: 250,
-	        targetHeight: 250,
-	        popoverOptions: CameraPopoverOptions,
-	        saveToPhotoAlbum: false
-	    };
-
-	    $cordovaCamera.getPicture(options).then(function(imageData) {
-	        $scope.srcImage = "data:image/jpeg;base64," + imageData;
-          CameraFactory.uploadImage($scope.srcImage);
-	    }, function(err) {
-	        console.error('Camera Error', err);
-	    });
+	$scope.changeStatus = function () {
+		$rootScope.groupLoggedIn = !($rootScope.groupLoggedIn);
 	};
+
+	$scope.captureImage = function() {
+	    var options = {
+	      limit: 1
+	    };
+	    $cordovaCapture.captureImage(options).then(function(imageData) {
+		    console.log("Raw output", imageData[0]);
+			toDataUrl(imageData[0].fullPath, function(base64Img){
+				CameraFactory.uploadMedia(base64Img, "photo")
+			})
+	    });
+
+	};
+
+    $scope.captureVideo = function() {
+        var options = {
+        	limit: 1
+        };
+
+        $cordovaCapture.captureVideo(options).then(function(videoData) {
+			toDataUrl(videoData[0].fullPath, function(base64Img){
+				CameraFactory.uploadMedia(base64Img, "video")
+			})
+            // CameraFactory.uploadImage($scope.srcImage);
+        }, function(err) {
+            console.error('Camera Error', err);
+        });
+    };
+
+	function toDataUrl(url, callback) {
+		var xhr = new XMLHttpRequest();
+		xhr.responseType = 'blob';
+		xhr.onload = function() {
+			var reader = new FileReader();
+			reader.onloadend = function() {
+			  callback(reader.result);
+			}
+			reader.readAsDataURL(xhr.response);
+		};
+		xhr.open('GET', url);
+		xhr.send();
+	}
+
 });
+
+angular.module('main').filter('trusted', ['$sce', function ($sce) {
+    return function(url) {
+        return $sce.trustAsResourceUrl(url);
+    };
+}]);
 
--- www/js/common/factories/camera.factory.js
+++ www/js/common/factories/camera.factory.js
@@ -5,24 +5,27 @@ angular.module('main')
 
   var ref = firebase.database().ref();
 
-  CameraFactory.uploadImage = function(imageData) {
+  CameraFactory.uploadMedia = function(mediaData, type) {
     if (!$rootScope.profile) return;
 
-    var file = Upload.dataUrltoBlob(imageData, $rootScope.profile.uid);
+    var file = Upload.dataUrltoBlob(mediaData, Date.now());
     var uploadPic = storageRef.child('pictures/' + file.name).put(file);
 
     uploadPic.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
       function(snapshot) {
         // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
         var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+
+        // leave in here for compression/speed testing
         console.log('Upload is ' + progress + '% done');
+
       }, function(err) {
         console.error(err);
-      }, function() {
-        // Upload completed successfully, now we can get the download URL
+      }, function() { 
         var downloadURL = uploadPic.snapshot.downloadURL;
         var mediaObj = {
           mediaUrl: downloadURL,
+          mediaType: type,
           memberId: $rootScope.profile.uid
         }
         //stores a reference to the download URL in the group's db
