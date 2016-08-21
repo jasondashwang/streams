b74fb4bfa0f9533c92055ab53c2327c7750ef103
Seon Joong Kim <seonjoongkim@college.harvard.edu>
2016-08-21 15:18:43 -0400

    added cancel button for sending media

--- www/js/group/sendMedia/sendMedia.js
+++ www/js/group/sendMedia/sendMedia.js
@@ -2,15 +2,19 @@ angular.module('main')
 	.controller("SendMediaCtrl", function($scope, $stateParams, CameraService, GroupFactory, CameraFactory, $state){
 
 	    $scope.$on("$ionicView.enter", function () {
-			$scope.media = CameraService.media
+			$scope.sendGroups = [];
+			$scope.media = CameraService.media;
 			GroupFactory.fetchCurrentGroups()
               .then(function(groups){
 				$scope.groups = groups;
               })	    	
 	    })
 
-
-		$scope.sendGroups = [];
+	    $scope.cancel = function () {
+	    	CameraService.media = null;
+	    	$scope.sendGroups = [];
+	    	$state.go("tab.groups")
+	    }
 
 		$scope.sendToGroups = function () {
 			CameraFactory.sendMedia($scope.sendGroups, $scope.media)
