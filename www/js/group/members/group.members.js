9312be81676c409c29a86f3555b29ddc9649bd47
Seon Joong Kim <seonjoongkim@college.harvard.edu>
2016-08-23 15:47:03 -0400

    filter box for maps

--- www/js/group/members/group.members.js
+++ www/js/group/members/group.members.js
@@ -6,6 +6,7 @@ angular.module('main').controller('GroupMembersCtrl', ['$scope', 'GroupService',
     GroupService.getGroupMembers($stateParams.groupCode)
     .then(function(members){
       $scope.members = members;
+      
     })
     .catch(function(err){
       console.err(err);
