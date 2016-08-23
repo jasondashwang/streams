c26d26987d5955a2e4a0fb9f7d8ef513e5af1792
Samuel Lam <samuelkinlam@gmail.com>
2016-08-23 14:24:45 -0400

    remove consoles

--- www/js/group/sendMedia/sendMedia.js
+++ www/js/group/sendMedia/sendMedia.js
@@ -1,36 +1,34 @@
 angular.module('main')
 	.controller("SendMediaCtrl", function($scope, $stateParams, CameraService, GroupService, CameraFactory, $state, $ionicHistory){
 
-	    $scope.$on("$ionicView.enter", function () {
-				$scope.media = CameraService.media;
-				$ionicHistory.nextViewOptions({
-				    disableBack: true
-				 });
-		    GroupService.getCurrentGroups()
-			  .then(function(groups) {
-			  	$scope.groups = [];
-			  	for (var group in groups) {
-			  		$scope.groups.push(groups[group]);
-			  	}
-			  })
-			  .catch(function(err){
-			    $.growl.error({location: 'tc', message: err.message});
-			  });
-			});
+    $scope.$on("$ionicView.enter", function () {
+			$scope.media = CameraService.media;
+			$ionicHistory.nextViewOptions({
+			    disableBack: true
+			 });
+	    GroupService.getCurrentGroups()
+		  .then(function(groups) {
+		  	$scope.groups = [];
+		  	for (var group in groups) {
+		  		$scope.groups.push(groups[group]);
+		  	}
+		  })
+		  .catch(function(err){
+		    $.growl.error({location: 'tc', message: err.message});
+		  });
+		});
 
-			function filterSendGroups() {
-				console.log('should have sendgroup prop', $scope.groups);
-				$scope.groups.forEach(function(group) {
-					delete group.sendGroup;
-				console.log('should have no sendgroup property', $scope.groups);
-			});
-			}
+		function filterSendGroups() {
+			$scope.groups.forEach(function(group) {
+				delete group.sendGroup;
+		});
+		}
 
 		var sendGroupCodes = [];
     $scope.cancel = function () {
     	CameraService.media = null;
     	filterSendGroups();
-    	sendGroupsCodes = [];
+    	sendGroupCodes = [];
     	$state.go("tab.camera");
     };
 
@@ -38,8 +36,6 @@ angular.module('main')
 			$scope.groups.forEach(function(group) {
 				if (group.sendGroup) sendGroupCodes.push(group.groupCode);
 			});
-			console.log(sendGroupCodes);
-			console.log('groups that are sending', sendGroupCodes);
 			CameraFactory.sendMedia(sendGroupCodes, $scope.media);
 			sendGroupCodes = [];
 			filterSendGroups();
